d0acb6d24de88694d5efba9951c0f12d0c13b4bd
Antonio Queb <antonioqueb@alphaqueb.online>
2024-11-16 13:23:08 -0600

    Up

--- __manifest__.py
+++ __manifest__.py
@@ -6,10 +6,10 @@
     'depends': ['base', 'sale_management', 'account', 'project'],
     'data': [
         'security/ir.model.access.csv',
-        'views/plan_suscripcion_views.xml',  # Define las acciones aquí primero
-        'views/contrato_suscripcion_views.xml',  # Define las acciones necesarias para contratos
-        'views/menu_views.xml',  # Carga los menús después de las acciones
-        'views/ticket_suscripcion_views.xml',
+        'views/plan_suscripcion_views.xml',  # Define las acciones de planes
+        'views/contrato_suscripcion_views.xml',  # Define las acciones de contratos
+        'views/ticket_suscripcion_views.xml',  # Define las acciones de tickets
+        'views/menu_views.xml',  # Define los menús
         'views/metrica_suscripcion_views.xml',
         'data/planes_suscripcion_data.xml',
         'data/cron_jobs.xml',
